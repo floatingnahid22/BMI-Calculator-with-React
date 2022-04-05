05ce03142a02299383bd2ead3022799af3600b40
floatingnahid22 <nahid.uap.github@gmail.com>
2022-04-06 00:10:55 +0600

    wrote jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './index.css';
 
 function App() {
+
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+  let imgSrc =''
+
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <div className='container'>
+        <h2 className='center'>BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight (lbs)</label>
+            <input value={weight} />
+          </div>
+          <div>
+            <label>Height (in)</label>
+            <input value={height} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline' type='submit'>Reload</button>
+          </div>
+        </form>
+
+        <div className='center'>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>{message}</p>
+        </div>
+
+        <div className='img-container'>
+          <img src={imgSrc} alt=''></img>
+        </div>
+      </div>
     </div>
   );
 }
